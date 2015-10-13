1a37055a3991a706919b1e70478673d693c1f4ce
DongHoon <dhdnzk0304@gmail.com>
2015-10-14 02:18:33 +0900

    전체 메소드 구현(아직 테스트x)

--- Nevigator.java
+++ Nevigator.java
@@ -3,11 +3,12 @@ package maze;
 /**
  * Created by DH on 2015. 10. 13..
  * Last modification 2015. 10. 13..
+ *
  */
 public class Nevigator {
 
     static int whereToGo ( int[][] map, Node curPosition ) {
-        if ( canGoRight( map, curPosition ) ) {
+        if ( canGoRight(map, curPosition) ) {
             return 1;
         }
         else if ( canGoDown( map, curPosition ) ) {
@@ -20,7 +21,7 @@ public class Nevigator {
             return 4;
         }
         else if ( canGoBack( map, curPosition ) ) {
-            return 5;   // go back
+            return 5;
         }
         else {
             return 6;   // game end
@@ -28,27 +29,67 @@ public class Nevigator {
     }
 
     static boolean canGoRight ( int[][] map, Node curPosition ) {
-
+        try {
+            if (map[curPosition.getyPos()][curPosition.getxPos() + 1] == 0)
+                return true;
+            else
+                return false;
+        }catch ( ArrayIndexOutOfBoundsException e ) {
+            e.getMessage();
+            return false;
+        }
     }
 
     static boolean canGoDown ( int[][] map, Node curPosition ) {
-
+        try {
+            if (map[curPosition.getyPos() + 1][curPosition.getxPos()] == 0)
+                return true;
+            else
+                return false;
+        } catch ( ArrayIndexOutOfBoundsException e ) {
+            e.getMessage();
+            return false;
+        }
     }
 
     static boolean canGoLeft ( int[][] map, Node curPosition ) {
-
+        try {
+            if (map[curPosition.getyPos()][curPosition.getxPos() - 1] == 0)
+                return true;
+            else
+                return false;
+        } catch ( ArrayIndexOutOfBoundsException e ) {
+            e.getMessage();
+            return false;
+        }
     }
 
     static boolean canGoUP ( int[][] map, Node curPosition ) {
-
+        try {
+            if (map[curPosition.getyPos()][curPosition.getxPos()] == 0)
+                return true;
+            else
+                return false;
+        } catch ( ArrayIndexOutOfBoundsException e ) {
+            return false;
+        }
     }
 
     static boolean canGoBack ( int[][] map, Node curPosition ) {
-
-    }
-
-    static boolean gameEndOrNot() {
-
-        return true;
+        try {
+            if (map[curPosition.getyPos()][curPosition.getxPos() + 1] == 2)
+                return true;
+            else if (map[curPosition.getyPos()][curPosition.getxPos() - 1] == 2)
+                return true;
+            else if (map[curPosition.getyPos() + 1][curPosition.getxPos()] == 2)
+                return true;
+            else if (map[curPosition.getyPos() - 1][curPosition.getxPos()] == 2)
+                return true;
+            else
+                return false;
+        } catch ( ArrayIndexOutOfBoundsException e ) {
+            e.getMessage();
+            return false;
+        }
     }
 }
