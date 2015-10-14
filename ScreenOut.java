703b3776c363066f4dcc3ad316138bde24d5d792
DongHoon <dhdnzk0304@gmail.com>
2015-10-14 22:04:39 +0900

    딜레이 속도 조절

--- ScreenOut.java
+++ ScreenOut.java
@@ -54,7 +54,7 @@ public class ScreenOut {
         }
         System.out.println("─┘");
         try {
-            Thread.sleep ( 1000 );
+            Thread.sleep ( 500 );
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
