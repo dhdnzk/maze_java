59c7a1f027c8c709a32451495b9d1db23651fbe7
DongHoon <dhdnzk0304@gmail.com>
2015-10-14 20:11:08 +0900

    미로찾기 구현, 테스팅중(발견된 남은 버그 : 3개)

--- Nevigator.java
+++ Nevigator.java
@@ -7,83 +7,83 @@ package maze;
  */
 public class Nevigator {
 
-    static int whereToGo ( int[][] map, Node curPosition ) {
-        if ( canGoRight(map, curPosition) ) {
+    static int whereToGo(int[][] map, Node curPosition) {
+        if (canGoRight(map, curPosition)) {
             return 1;
-        }
-        else if ( canGoDown( map, curPosition ) ) {
+        } else if (canGoDown(map, curPosition)) {
             return 2;
-        }
-        else if ( canGoLeft( map, curPosition ) ) {
+        } else if (canGoLeft(map, curPosition)) {
             return 3;
-        }
-        else if ( canGoUP( map, curPosition ) ) {
+        } else if (canGoUP(map, curPosition)) {
             return 4;
-        }
-        else if ( canGoBack( map, curPosition ) ) {
+        } else if (canGoBack(map, curPosition)) {
             return 5;
-        }
-        else {
+        } else {
             return 6;   // game end
         }
     }
 
-    static boolean canGoRight ( int[][] map, Node curPosition ) {
+    static boolean canGoRight(int[][] map, Node curPosition) {
         try {
-            if (map[curPosition.getyPos()][curPosition.getxPos() + 1] == 0)
-                return true;
-            else
-                return false;
-        }catch ( ArrayIndexOutOfBoundsException e ) {
+            if (curPosition.getxPos() < map[0].length - 1) {
+                if (map[curPosition.getyPos()][curPosition.getxPos() + 1] == 0)
+                    return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             e.getMessage();
             return false;
         }
+        return false;
     }
 
-    static boolean canGoDown ( int[][] map, Node curPosition ) {
+    static boolean canGoDown(int[][] map, Node curPosition) {
         try {
-            if (map[curPosition.getyPos() + 1][curPosition.getxPos()] == 0)
-                return true;
-            else
-                return false;
-        } catch ( ArrayIndexOutOfBoundsException e ) {
+            if (curPosition.getyPos() < map.length - 1) {
+                if (map[curPosition.getyPos() + 1][curPosition.getxPos()] == 0)
+                    return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             e.getMessage();
             return false;
         }
+        return false;
     }
 
-    static boolean canGoLeft ( int[][] map, Node curPosition ) {
+    static boolean canGoLeft(int[][] map, Node curPosition) {
         try {
-            if (map[curPosition.getyPos()][curPosition.getxPos() - 1] == 0)
-                return true;
-            else
-                return false;
-        } catch ( ArrayIndexOutOfBoundsException e ) {
+            if (0 < curPosition.getxPos()) {
+                if (map[curPosition.getyPos()][curPosition.getxPos() - 1] == 0)
+                    return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             e.getMessage();
             return false;
         }
+        return false;
     }
 
-    static boolean canGoUP ( int[][] map, Node curPosition ) {
+    static boolean canGoUP(int[][] map, Node curPosition) {
         try {
-            if (map[curPosition.getyPos() - 1][curPosition.getxPos()] == 0)
-                return true;
-            else
-                return false;
-        } catch ( ArrayIndexOutOfBoundsException e ) {
+            if (0 < curPosition.getyPos()) {
+                if (map[curPosition.getyPos() - 1][curPosition.getxPos()] == 0)
+                    return true;
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
             return false;
         }
+        return false;
     }
+
 
     // 벽에 닿았을때 선언된 배열 범위를 넘어가는지 경계검사 해야함
     static boolean canGoBack ( int[][] map, Node curPosition ) {
         try {
-            if (curPosition.getxPos() < map[0].length) {
+            if ( curPosition.getxPos() < map[0].length -1) {
                 if (map[curPosition.getyPos()][curPosition.getxPos() + 1] == 2)
                     return true;
             }
 
-            if (curPosition.getyPos() < map.length) {
+            if (curPosition.getyPos() < map.length - 1) {
                 if (map[curPosition.getyPos()][curPosition.getxPos() - 1] == 2)
                     return true;
             }
